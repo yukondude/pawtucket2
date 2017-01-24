703b5c2f23996f67449f77dd0352c978955c47cd
hosting <hosting@collectiveaccess.org>
2017-01-24 17:44:51 +0000

    Add Arlington theme

---
+++ themes/apl_clh_custom/views/Browse/Index.php
@@ -0,0 +1,17 @@
+<?php
+	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": Browse");
+?>
+<H1><?php print _t("Browse"); ?></H1>
+<div class="row">
+	<div class="col-sm-6 col-md-6">
+		<p>browse test page</p>
+	</div>
+	<div class="col-sm-6 col-md-6">
+		<p><b>Contact Us</b></p>
+		<p>Central Library - Main Floor</p>
+		<p>1015 N. Quincy St.</p>
+		<p>Arlington, VA 22201</p>
+		<p>Phone: (703) 228-5956</p>
+		<p>Email: localhistory@arlingtonva.us</p>
+	</div>
+</div>
