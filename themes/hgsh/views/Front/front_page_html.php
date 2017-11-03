f33f2241563549abcca898f2b4b9d21ac5d367a1
mwbenowitz <mwbenowitz@gmail.com>
2017-11-03 10:10:38 -0400

    Fixed Front Page Link

--- themes/hgsh/views/Front/front_page_html.php
+++ themes/hgsh/views/Front/front_page_html.php
@@ -53,7 +53,7 @@
 					<?php print caGetThemeGraphic($this->request, 'spacer.png'); ?>
 				</div>
 				<div class="hpText">
-					Hampstead Garden Suburb, near London’s Hampstead Heath, is a vibrant community set within a unique mix of open spaces, planned street scene, and internationally recognised architecture. Welcome to our Virtual Museum.<br/><?php print caNavLink($this->request, 'See Guide', '', '', 'About', 'museum_help'); ?>
+					Hampstead Garden Suburb, near London’s Hampstead Heath, is a vibrant community set within a unique mix of open spaces, planned street scene, and internationally recognised architecture. Welcome to our Virtual Museum.<br/><?php print caNavLink($this->request, 'See Guide', '', '', 'About', 'museum_guide'); ?>
 				</div>
 			</div><!--end col-sm-8-->
 			</div>
