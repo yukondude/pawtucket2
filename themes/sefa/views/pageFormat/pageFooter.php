179cd909382c683d1b893513146e57147b63a855
Maria Passarotti <maria@whirl-i-gig.com>
2018-03-30 13:48:15 -0400

    sefa

--- themes/sefa/views/pageFormat/pageFooter.php
+++ themes/sefa/views/pageFormat/pageFooter.php
@@ -37,6 +37,7 @@
 					<?php print caNavLink($this->request, _t("Newsletter Signup"), "footerButton", "", "About", "MailingList"); ?><br/>
 					<a href="https://www.facebook.com/pages/Susan-Eley-Fine-Art/137980986325025" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'fbook.png'); ?></a>
 					<a href="https://twitter.com/EleyFineArt" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'twitter.png'); ?></a>
+					<a href="https://instagram.com/sefa_gallery" class="socialicon"><?php print caGetThemeGraphic($this->request, 'instagram.png'); ?></a>
 					<a href="https://artsy.net/susan-eley-fine-art?utm_source=follow_badge" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'artsy.png'); ?></a>
 					<a href="https://www.1stdibs.com/dealers/susan-eley-fine-art/?utm_source=susaneleyfineart.com&utm_medium=referral&utm_campaign=dealer&utm_content=dealer_badge_1stdibs_gray" target="_blank" class="socialicon"><?php print caGetThemeGraphic($this->request, '1stdibs.png', array("title" => "Shop Susan Eley Fine Art's fine art on 1stdibs")); ?></a>
 					<a href="mailto:susie@susaneleyfineart.com" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'envelope.png'); ?></a>
@@ -45,6 +46,7 @@
 					<?php print caNavLink($this->request, _t("Newsletter Signup"), "footerButton", "", "About", "MailingList"); ?><br/><br/>
 					<a href="https://www.facebook.com/pages/Susan-Eley-Fine-Art/137980986325025" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'fbook.png'); ?></a>
 					<a href="https://twitter.com/EleyFineArt" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'twitter.png'); ?></a>
+					<a href="https://instagram.com/sefa_gallery" class="socialicon"><?php print caGetThemeGraphic($this->request, 'instagram.png'); ?></a>
 					<a href="https://artsy.net/susan-eley-fine-art?utm_source=follow_badge" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'artsy.png'); ?></a>
 					<a href="https://www.1stdibs.com/dealers/susan-eley-fine-art/?utm_source=susaneleyfineart.com&utm_medium=referral&utm_campaign=dealer&utm_content=dealer_badge_1stdibs_gray" target="_blank" class="socialicon"><?php print caGetThemeGraphic($this->request, '1stdibs.png', array("title" => "Shop Susan Eley Fine Art's fine art on 1stdibs")); ?></a>
 					<a href="mailto:susie@susaneleyfineart.com" class="socialicon gray"><?php print caGetThemeGraphic($this->request, 'envelope.png'); ?></a>
